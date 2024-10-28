38cfd4cf322dfdc10eb889515fe430298ee04ec4
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-10-28 21:33:08 +0300

    Сделал собщение на команды `start`

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
+const { Bot, InputFile } = require("grammy")
+const message = require("./message")
 const config = require('./config.js')
+
+const bot = new Bot(config.bot_token)
+
+
+//Приветствие новых пользователей
+bot.command(`start`, (ctx) => {
+    console.log('Команда /start получена');
+    const gifStart = `picture/moneta.gif`
+
+    ctx.replyWithAnimation( new InputFile(gifStart), {caption: message.start, parse_mode: "MarkdownV2" })
+})
+
+bot.start()
---
+++ message.js
@@ -0,0 +1,5 @@
+const message =  {
+    start: "*Добро пожаловать в Penni bot*\nСо мной можно играть в монетку\nи смотреть свои результаты\\."
+}
+
+module.exports = message;
