9677742c004bc0549ba0f8e2651928f49612b1bc
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-11-02 11:50:48 +0300

    Добавление .env

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
+require('dotenv').config();
+
 const config = {
-    bot_token: "7715265001:AAH0AQHpGkdogV2RzGnLNL54Of7pqzTjccg",
+bot_token: process.env.BOT_TOKEN,
     firebaseConfig: {
-        apiKey: "AIzaSyBZhalv8DUyscWl1dvs5mk6hkfc7MvsmA4",
-        authDomain: "pennibot-254f6.firebaseapp.com",
-        databaseURL: "https://pennibot-254f6-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "pennibot-254f6",
-        storageBucket: "pennibot-254f6.appspot.com",
-        messagingSenderId: "520844607032",
-        appId: "1:520844607032:web:d98ed5c589bc33cd8fb0cd",
-        measurementId: "G-633LDVBYC1"
+        apiKey: process.env.DATABASE_API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
     },
     firebaseAuth: {
-        mail: "arseniymironov3116@yandex.ru",
-        password: "Kroko12345"
+        mail: process.env.EMAIL,
+        password: process.env.PASSWORD,
     },
-    databaseURL: "https://pennibot-254f6-default-rtdb.europe-west1.firebasedatabase.app/",
+    databaseURL: process.env.DATABASE_URL,
 }
 
 module.exports = config
